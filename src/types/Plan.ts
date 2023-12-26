a33e775c49ee730ee9b9897dad147e221e1a36d7
taraldgasbakk <taraldga@gmail.com>
2023-12-26 09:07:30 +0100

    Update WorkoutPlans to use the correct fields

--- src/types/Plan.ts
+++ src/types/Plan.ts
@@ -1,11 +1,26 @@
 
-export interface WorkoutSummary {
+export interface ExerciseLoad {
+    reps: number;
+    sets: number;
+    weight: number;
+    isBodyWeight: boolean;
+}
+
+export interface Exercice {
+    exerciseId: string;
+    name: string;
+    link: string;
+    description: string;
+    load: ExerciseLoad;
+}
+export interface Workout {
     title: string;
     description: string;
     link: string;
+    exercises: Exercice[]
 }
 export interface Plan {
     title: string;
     notes: string;
-    weeks: WorkoutSummary[][]
+    workouts: Workout[]
 }
