d0e013b67ac8b61eb26704a53453f0b630b7ad18
Tarald <taraldga@gmail.com>
2024-12-16 10:10:17 +0100

    Go from using modules to imports so that we can use higher node versions

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+
+import TailwindAnimate from 'tailwindcss-animate';
+
+export default {
   darkMode: ["class"],
   content: [
     './pages/**/*.{ts,tsx}',
@@ -73,5 +76,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [TailwindAnimate],
 }
